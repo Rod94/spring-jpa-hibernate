93e54ded82c4543217b721c02811c53a20264a8b
Rodolpho Caetano <rphfti@hotmail.com>
2022-08-04 12:15:54 -0300

    User entity and resource

---
+++ src/main/java/com/rodolpho/study/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.rodolpho.study.resources;
+
+import com.rodolpho.study.entities.User;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria123@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
